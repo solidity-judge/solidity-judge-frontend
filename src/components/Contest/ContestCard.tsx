1d3fba3e38d5f4bfd4b08252827127053b94bd4e
Tran Duy Tien <tranduytienkh@gmail.com>
2023-05-28 10:47:05 +0700

    Add single contest page (#16)

--- src/components/Contest/ContestCard.tsx
+++ src/components/Contest/ContestCard.tsx
@@ -1,9 +1,27 @@
+import { useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { ContestPreview } from "types/Contest";
 
 export default function ContestCard({ contest }: { contest: ContestPreview }) {
   const navigate = useNavigate();
+  const deadline = contest.deadline;
+  const deadlineRef = useRef<HTMLSpanElement>(null);
 
+  useEffect(() => {
+    if (deadlineRef.current) {
+      const interval = setInterval(() => {
+        const secondsRemaining = deadline - Math.floor(Date.now() / 1000);
+        const days = Math.floor(secondsRemaining / (3600 * 24));
+        const hours = Math.floor((secondsRemaining - days * 3600 * 24) / 3600);
+        const minutes = Math.floor(
+          (secondsRemaining - days * 3600 * 24 - hours * 3600) / 60
+        );
+        const seconds = secondsRemaining % 60;
+        deadlineRef.current!.innerText = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+      }, 1000);
+      return () => clearInterval(interval);
+    }
+  }, []);
   const handleClick = () => {
     navigate(`/contests/${contest.id}`);
   };
@@ -21,6 +39,9 @@ export default function ContestCard({ contest }: { contest: ContestPreview }) {
       <div className={"border-l"}></div>
       <div>
         <div className="font-medium">{contest.title}</div>
+      </div>
+      <div className="ml-auto flex flex-col justify-center">
+        <span className="text-sm font-medium" ref={deadlineRef}></span>
       </div>
     </div>
   );
